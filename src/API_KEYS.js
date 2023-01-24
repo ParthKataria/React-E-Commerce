40a9bc42d51511b6a52e1cc0d6c59f91343cf3bf
ParthKataria <70621812+ParthKataria@users.noreply.github.com>
2023-01-24 17:00:22 +0530

    Delete API_KEYS.js

--- src/API_KEYS.js
+++
@@ -1,5 +0,0 @@
-const headers = {
-  "X-RapidAPI-Key": "864dac725amshac87f811e37cacdp1def16jsn271d0e0550d4",
-  "X-RapidAPI-Host": "apidojo-hm-hennes-mauritz-v1.p.rapidapi.com",
-};
-export { headers };
